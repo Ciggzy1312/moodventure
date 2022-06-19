f8ea145458bc7503f03977f77dab2086c1e7cf99
Deepayan <deepayanmukherjee1312@gmail.com>
2022-06-19 17:48:36 +0530

    Minor updates 4.0
    Signed-off-by: Deepayan <deepayanmukherjee1312@gmail.com>

--- components/Calendar.js
+++ components/Calendar.js
@@ -110,7 +110,7 @@ const Calendar = ({ items }) => {
         </div>
 
         <div className=''>
-          <Link><a href="/api/auth/logout" className='text-[#2FCC9D] hover:text-white'><button className=' border-[#2FCC9D] border-2 rounded font-semibold px-4 py-1.5 hover:bg-[#2FCC9D]'>Logout</button></a></Link>
+          <Link href="/api/auth/logout" className=''><button className=' border-[#2FCC9D] border-2 rounded font-semibold px-4 py-1.5 hover:bg-[#2FCC9D] text-[#2FCC9D] hover:text-white'>Logout</button></Link>
         </div>
       </div>
 
